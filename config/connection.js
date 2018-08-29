df11942b019abdb80af7f0ba30b17eaa86d3fc3d
NickMcD4 <nickmcd4@gmail.com>
2018-08-28 23:21:11 -0600

    added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,18 @@ var mysql = require('mysql');
 
 if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
-var connection = mysql.createConnection({
-    host: "	s554ongw9quh1xjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "	a6us1r9x3p681n9u",
-    password: "mizati964pvd1lp9",
-    database: "	burger_db"
-});
+
+}else{
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Tomwatson123",
+        database: "burger_db"
+    });
 }
+
+
 connection.connect(function(err){
     if (err){
         console.error("error connecting to database: " + err.stack);
